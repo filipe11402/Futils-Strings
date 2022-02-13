0323d557387f864b9d8471c72f733e0d817f6c08
filipe11402 <filipe11402@outlook.pt>
2022-02-13 22:55:40 +0000

    added skeleton for enum conversion

---
+++ Futils.Strings.src/Extensions/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Futils.Strings.src.Extensions
+{
+    public static class EnumExtensions<T>
+    {
+        public static T GetEnum<T>(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+    }
+}
